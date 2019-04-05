4ae5c4bbbbd642124f20087bc60261071a33630a
Manmeet Singh <35901948+manmeetsingh7781@users.noreply.github.com>
2019-04-05 11:51:34 -0700

    Update perfectNumbers.cpp

--- perfectNumbers.cpp
+++ perfectNumbers.cpp
@@ -19,115 +19,111 @@ int main()
    isPerfect(LIMIT, true);
    cout << endl;
    
+   isAbundent(LIMIT, true);
+   cout << endl;
    // This function calculates the total Percentage of Numbers
    getTotalPercentage();
-   
+
+   cout << endl;
    return 0;
 }
 
+// Checks if the number is perfect
 void isPerfect(int to, bool print ){
     
-    int startChecking, start_point;
+    int startChecking, start_point, sum;
+    total_perfect = 0;
 
-    bool isDivisible;
-    if(print){
-    cout << "What numbers below " << LIMIT << " are perfect?\n";
-    }
+    // print numbers to screen 
+    if(print)
+      cout << "What numbers below " << LIMIT << " are perfect?\n";
+    
+
     for(start_point  = 1; start_point <= to; start_point++){
-      
         // Every Number will have its different sum
-        int sum = 0;
-        
+        sum = 0;
+      
       // Start checking the perfect number 
         for(startChecking = 1; startChecking <= start_point/2; startChecking++){
-                isDivisible = start_point % startChecking;
-                if(not isDivisible){
+                if(start_point % startChecking == 0)
                     // Adding Dividors 
                     sum += startChecking;
-                }
             }
             // Checks the sum of the number;
             if(sum == start_point){
-                if(print){
-                    cout << start_point << endl;
-                }
-                total_perfect+=1;
-                
-            } 
+                total_perfect++;
+                if(print)
+                  cout << start_point << endl;
+          } 
     }
 }
 
-
+// Checks if the number is Abundent
 void isAbundent(int to, bool print ){
-     
-    int startChecking, start_point;
+    int startChecking, start_point, sum;
+    total_abdundent = 0;
 
-    bool isDivisible;
-    if(print){
-    cout << "What odd numbers below "<< LIMIT <<" are abundant?\n";
-    }
+    if(print)
+      cout << "What odd numbers below "<< LIMIT <<" are abundant?\n";
+    
     for(start_point  = 1; start_point <= to; start_point+=2){
         // Every Number will have its different sum
-        int sum = 0;
+        sum = 0;
       // Start checking the perfect number 
         for(startChecking = 1; startChecking <= start_point/2; startChecking++){
-                isDivisible = start_point % startChecking;
-                if(not isDivisible){
+                if(start_point % startChecking == 0)
                     // Adding Dividors 
                     sum += startChecking;
-                }
             }
-            
             // Checks the sum of the number;
            if(sum > start_point){
-                total_abdundent+=1;
-               if(print){
-                cout << start_point << endl;
-            }
-               
+              total_abdundent++;
+               if(print)
+                cout << start_point << endl; 
            }
     }
 }
 
-
+// checks if the number is Deficient
 void isDeficient( int to, bool print ){
-    int startChecking, start_point;
-    bool isDivisible;
-    if(print){
-    cout << "The Deficient Numbers are below " << LIMIT << " : " ;
-    }
+
+    int startChecking, start_point, sum;
+    total_deficient = 0;
+    if(print)
+      cout << "The Deficient Numbers are below " << LIMIT << " : " ;
     for(start_point  = 1; start_point <= to; start_point++){
       
         // Every Number will have its different sum
-        int sum = 0;
+        sum = 0;
       // Start checking the perfect number 
         for(startChecking = 1; startChecking <= start_point/2; startChecking++){
-                isDivisible = start_point % startChecking;
-                if(not isDivisible){
+                if(start_point % startChecking == 0)
                     // Adding Dividors 
                     sum += startChecking;
-                    
-                }
             }
             // Checks the sum of the number;
         if (sum < start_point){
-                total_deficient+=1;
-         if(print){
+                total_deficient++;
+         if(print)
                 cout << start_point << endl;
-            }
-                
         }
     }
 }
 
 // This function calculates the total Percentage of Numbers
 void getTotalPercentage(){
+
+    // calling the functions and gathering information
     isPerfect(LIMIT, false);
     isAbundent(LIMIT, false);
     isDeficient(LIMIT,false);
+
+    // calculates the total
     total_perfect = (total_perfect/LIMIT)*100;
     total_abdundent = (total_abdundent/LIMIT)*100;
     total_deficient = (total_deficient/LIMIT)*100;
+
+    // prints the total to screen
     cout << "The Percentage of Numbers are: \n";
     cout << "Perfect: " << total_perfect <<"%" << "\nAbundant: " <<  total_abdundent <<"%"<< "\nDeficient: " << total_deficient <<"%" << endl;
 }
