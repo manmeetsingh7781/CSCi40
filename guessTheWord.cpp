9d57a0e060c1175b0d530eaa9a4704ac2783c718
Manmeet Singh <35901948+manmeetsingh7781@users.noreply.github.com>
2019-04-05 18:13:58 -0700

    Update guessTheWord.cpp

--- guessTheWord.cpp
+++ guessTheWord.cpp
@@ -4,72 +4,85 @@
 #include <iostream>
 using namespace std;
 
+// function prototypes
 void playGame();
+char userResponse();
+void gameReset();
 
-
+// game declarations
+bool gameOver;
+int guessCount;
+char first, last, center, response; 
+    
+// Main thread
 int main() {
-    playGame();
-    return 0;
+  playGame();
+  cout << endl;
+  return 0;
 }
 
+// Main game function 
 void playGame(){
-    bool gameOver;
-    int guessCount;
-    char first, last, center;
-    string response; 
-    
-    gameOver = false;
-    guessCount = 5;
-    first = 'A';
-    last = 'Z';
-    
-    center = (first + last)/2;
-    center++;
-    
-while(guessCount != 0 and not gameOver){
-    
+ 
+  // Setting up the game
+  gameReset();
+
+  // While conditions are false game will be running 
+  while(guessCount != 0 and not gameOver){
+ 
     cout << "==============================================\n";
     cout << "Guess Counts: " << guessCount << "           " << endl;
     cout << "Is letter '" << center << "' you thought of?\n";
     cout << "==============================================\n\n";
-        
-    getline(cin, response);
-        
-    if(response == "y" or response == "yes"){
+
+   // If user wins
+    if(userResponse()  == 'Y'){
         cout << "I Won!, You lose.\n";
         cout << "Play Again?\n";
-        getline(cin, response);
-        if(response =="y" or response =="yes"){
-            gameOver = false;
-            guessCount = 6;
-        }else{
-            gameOver = true;
-        }
+      if(userResponse() == 'Y'){
+        gameReset();
+        guessCount+=1;
+      }else gameOver = true;
+
+    // if user does not win
     }else{
-            
-        cout << "The Letter you guess is after the letter '" << center <<"' ? \n\n";
-        getline(cin, response);
-            
-        if(response == "y" or response == "yes"){
+         cout << "The Letter you guess is after the letter '" << center <<"' ? \n\n";
+         userResponse();
+         if(response == 'Y'){
             first = center+1;
             center = (first+last)/2;
-        }
-    
-        if(response == "n" or response == "no"){
+        }else if(response == 'N'){
             last = center;
             center = (first + last)/2;
         }
     }
-    guessCount--;
+    // decreasing the guess counts each time user takes a turn
+    guessCount-=1;
     if(guessCount == 0) {
         cout << "Snap.... Looks like I ran out of guesses... You won! Press P to play again\n";
-        getline(cin, response);
         // Reseting the game 
-        if(response == "p"){
-            guessCount = 5;
-            first = 'A';
-            last = 'Z';
-        }
+        if(userResponse() == 'P')
+          gameReset();
     }
+  }
 }
+
+// this function set's up the game settings 
+void gameReset(){
+    gameOver = false;
+    guessCount = 5;
+    first = 'A';
+    last = 'Z';
+    center = (first + last)/2;
+    center++;      
+}
+
+// This functions gets the user response 
+char userResponse(){
+  // clears the input before using it
+  cin.clear();
+  response = cin.get();
+  cin.ignore(999, '\n');
+  response = toupper(response);
+  return response;
 }
