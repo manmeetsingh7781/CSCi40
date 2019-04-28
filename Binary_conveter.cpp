8bc75decdb3defd510e88594c1b079da5f5d8f94
Manmeet Singh <35901948+manmeetsingh7781@users.noreply.github.com>
2019-04-28 15:42:03 -0700

    Update Binary_conveter.cpp

--- Binary_conveter.cpp
+++ Binary_conveter.cpp
@@ -11,12 +11,14 @@ void base_converter(int, int);
 void base_converter(int, int, int);
 unsigned long long int binary_to_decimal(string binary);
 
+
 int main()
 {
-	base_converter(4500, 10, 16);
+	base_converter(1245, 10, 13);
 	cout << endl;
 	return 0;
 }
+
 // Takes a Binary and convert it into the decimal
 unsigned long long int binary_to_decimal(string binary) {
 	string digits = binary;
@@ -30,6 +32,7 @@ unsigned long long int binary_to_decimal(string binary) {
 	}
 	return sum;
 }
+
 // Takes a Decimal Number and converts the Base 2 upto 11
 void base_converter(int digits, int base) {
 	if (base > 1 and base < 37) {
@@ -51,20 +54,36 @@ void base_converter(int digits, int base) {
 
 // This function takes a number and from which base you want to convert the number into new base
 void base_converter(int number, int from_base, int to_base) {
+
 	if (from_base > 1 and from_base < 37 and to_base > 1 and to_base < 37) {
 		int to_placer = 0, power = 0, answer = 0;
 		int to_results[10];
+
 		while (number != 0) {
 			answer += static_cast<int>(number % 10 * pow(from_base, power++));;
 			number /= 10;
 		}
-		cout << "The answer is " << answer << endl;
 		while (answer != 0) {
 			to_results[to_placer++] = answer % to_base;
 			answer /= to_base;
 		}
 		for (power = to_placer - 1; power >= 0; power--) {
-			cout << to_results[power];
+			if (to_base == 13) {
+				switch (to_results[power]) {
+				case 10:
+					cout << "A";
+					break;
+				case 11:
+					cout << "B";
+					break;
+				case 12:
+					cout << "C";
+					break;
+				default:
+					cout << to_results[power];
+					break;
+				}
+			}
 		}
 	}
 	else { cout << "Base Limit has been exceeded, please try between 2 and 36\n" << endl;  exit(-1); }
