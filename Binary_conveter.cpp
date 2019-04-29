71a24b6fb163750250cfdb43882d0ba976252d83
Manmeet Singh <35901948+manmeetsingh7781@users.noreply.github.com>
2019-04-29 11:31:02 -0700

    Update Binary_conveter.cpp

--- Binary_conveter.cpp
+++ Binary_conveter.cpp
@@ -1,9 +1,7 @@
-
-#include "stdafx.h"
 // Manmeet Singh
 // Binary.cpp : This file contains the 'main' function. Program execution begins and ends there.
 
-
+#include "stdafx.h"
 #include <iostream>
 #include <cmath>
 #include <string>
