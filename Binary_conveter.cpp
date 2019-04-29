7a259d115ff32bf4de38696955d599bda63fd37d
Manmeet Singh <35901948+manmeetsingh7781@users.noreply.github.com>
2019-04-29 16:05:55 -0700

    Update Binary_conveter.cpp

--- Binary_conveter.cpp
+++ Binary_conveter.cpp
@@ -1,41 +1,32 @@
 // Manmeet Singh
 // Binary.cpp : This file contains the 'main' function. Program execution begins and ends there.
 
-#include "stdafx.h"
+#include "pch.h"
 #include <iostream>
 #include <cmath>
 #include <string>
 using namespace std;
 
-// Function Prototypes
-void getChar(int, int);
+// function Prototypes
+void getChar(int);
+int getInt(char);
 void base_converter(string, int, int);
-unsigned long long int binary_to_decimal(string binary);
 
 
 int main()
 {
+
 	char response;
 	int from, to;
 	string binary_value, value;
 	bool Done = false;
 	cout << cout.width(20) << "\bWelcome to Binary n Base Conveter\n" << endl;
 	while (not Done) {
-		cout << "A. Change Binary to Decimal.\n";
-		cout << "B. Change Decimal to another Base.\n>";
-		cin >> response;
-		response = toupper(response);
-		if (response == 'A') {
-			cout << "Enter The Number\n>";
-			cin >> binary_value;
-			cout << binary_to_decimal(binary_value) << endl;
-		}
-		else if (response == 'B') {
-			cout << "Please Enter Number, From Base, and To Base.\n>";
-			cin >> value >> from >> to;
-			base_converter(value, from, to);
-			cout << endl;
-		}
+		cout << cout.width(20)<< "\bChange One Base to Another Base.\n\n";
+		cout << "Please Enter Number, From Base, and To Base.\n>";
+		cin >> value >> from >> to;
+		base_converter(value, from, to);
+		cout << endl;
 		cout << "Convert Again? Y/N\n>";
 		cin >> response;
 		response = toupper(response);
@@ -49,7 +40,7 @@ int main()
 }
 
 // This function returns the Alphabetic characters based on Base number
-void getChar(int digit, int base) {
+void getChar(int digit) {
 	char letter;
 	if (digit > 9 and digit < 36) {
 		letter = digit + 55;
@@ -59,18 +50,14 @@ void getChar(int digit, int base) {
 	cout << digit;
 	return;
 }
-// Takes a Binary and convert it into the decimal
-unsigned long long int binary_to_decimal(string binary) {
-	string digits = binary;
-	int start = digits.length() - 1, each_number = 0, temper = 0, power = 0;
-	unsigned long long int sum = 0;
-	while (start >= 0) {
-		each_number = static_cast<int>(digits[start] - '0');
-		temper = static_cast<int>(each_number * pow(2, power++));
-		sum += temper;
-		start--;
+
+int getInt(char letter) {
+	int number;
+	if (letter >= 'A' and letter <= 'Z') {
+		number = letter - 55;
+		return number;
 	}
-	return sum;
+	return letter;
 }
 
 /*
@@ -82,15 +69,19 @@ This means max digit can be Base - 1
 // This function takes a number and from which base you want to convert the number into new base
 void base_converter(string number, int from_base, int to_base) {
 	int *to_results = NULL;
-	int SIZE = number.length() - 1, i = SIZE, from_power = 0, to_tenth_sum = 0, to_tenth_remainders = 0,  to_placer = 0, looper = 13, power = 0, answer = 0;
+	int SIZE = number.length() - 1, i = SIZE, from_power = 0, to_tenth_sum = 0, to_tenth_remainders = 0, to_placer = 0, looper = 13, power = 0, answer = 0;
 	to_results = new int[SIZE];
 	if (from_base > 1 and from_base < 37 and to_base > 1 and to_base < 37) {
 		while (i > -1) {
-			if (from_base < 13 and (number[i] - '0') < from_base) {
-				to_tenth_sum += static_cast<int>(number[i] - '0') * pow(from_base, from_power++);
+			if (from_base > 10 and number[i] > 10 and getInt(number[i]) < from_base) {
+				cout << getInt(number[i]);
+			}
+			else if ((number[i] - '0') < from_base) {
+				to_tenth_sum += static_cast<int>((number[i] - '0') * pow(from_base, from_power++));
+
 			}
 			else {
-				cout << "Number '" << number[i] << "' is Higher then Home Base. " << endl;
+				cout << "Number '" << number[i] << "' is Higher then Home Base: " << from_base << endl;
 				break;
 			}
 			i--;
@@ -100,8 +91,8 @@ void base_converter(string number, int from_base, int to_base) {
 			to_tenth_sum /= to_base;
 		}
 		for (power = to_placer - 1; power >= 0; power--) {
-			if (to_base > 12)
-				getChar(to_results[power], to_base);
+			if (to_base > 10)
+				getChar(to_results[power]);
 			else cout << to_results[power];
 		}
 	}
