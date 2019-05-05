e4670ffeef5e23924364a55311528214628552d8
Manmeet Singh <35901948+manmeetsingh7781@users.noreply.github.com>
2019-05-05 12:34:21 -0700

    Create Study_Guide.cpp

---
+++ Study_Guide.cpp
@@ -0,0 +1,26 @@
+// Manmeet Singh CSCI-40 Study Guide
+/*  
+	======== WRITE AND RUN ONCE ONLY =======
+
+1. Create a Linked List of nodes 26
+2. Adds value in Linked list
+3. Print the Link List
+4. Create a File and write youre name in it
+5. Read your name from the file.
+6. Write the factorial function using recursion
+
+	====== NOTE BOOK =======
+7. Create a addition table of base 6
+8. Convert 12F78 from base 18 to 10
+9. Count from 1 to 10 in base 14
+
+*/
+
+#include "pch.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	return 0;
+}
