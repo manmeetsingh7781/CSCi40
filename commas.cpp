ffdf521169b16f142003c02ec6992b7b0e2984dd
Manmeet Singh <35901948+manmeetsingh7781@users.noreply.github.com>
2019-09-19 20:18:28 -0700

    Update commas.cpp

--- commas.cpp
+++ commas.cpp
@@ -1,35 +1,45 @@
+//Manmeet Singh
+// This program will get input integers from user and print it with the commas
+
 #include <iostream>
 using namespace std;
 
 
 int main()
-{    
-    int original , temp, ones, ones1, ones2, ones3, temp2 , temp3 , thous, thous1, thous2, thous3, mills;
+{
+
+    int original , temp, ones, ones1, ones2, temp2 , temp3 , thous, thous1, thous2, mills;
     
-    cout << "Enter 7 to 9 number: ";
+    cout << "Enter 4 to 6 number: ";
     cin >> original;
     
-    temp = original/1000;
-    ones = original%1000;
+    temp = original/100;
+    ones = original%100;
     
     temp2 = ones/10;
     ones1 = ones%10;
     ones2 = temp2%10;
-    ones3 = temp2/10;
     
-    mills = temp/1000;
-    thous = temp%1000;
+    mills = temp/100;
+    thous = temp%100;
     
     temp3 = thous/10;
     thous1 = thous%10;
     thous2 = temp3%10;
-    thous3 = temp3/10;
     
     cout << "With Commas: \n";
-    cout << mills << ","<<thous3 << thous2 << thous1 <<"," << ones3 << ones2 << ones1 << endl;
+    cout << mills << ","<< thous2 << thous1 <<"," <<  ones2 << ones1 << endl;
 
     return 0;
 }
+
+
+
+
+
+
+
+
 
 
 
